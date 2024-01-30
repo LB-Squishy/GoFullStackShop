c39ed004db94c75f838e0efa5aae24548cb7a9a5
Laurent Le Boulbar <laurentleboulbar@gmail.com>
2024-01-30 15:03:00 +0100

    ajout du PUT & DELETE

--- backend/app.js
+++ backend/app.js
@@ -31,6 +31,20 @@ app.post('/api/stuff', (req, res, next) => {
     .catch(error => res.status(400).json({ error }));
 });
 
+// Route PUT
+app.put('/api/stuff/:id', (req, res, next) => {
+  Thing.updateOne({ _id: req.params.id }, {...req.body, _id: req.params.id})
+    .then(() => res.status(200).json({message: "Objet modifié !"}))
+    .catch(error => res.status(400).json({ error }));
+});
+
+// Route DELETE
+app.delete('/api/stuff/:id', (req, res, next) => {
+  Thing.deleteOne({ _id: req.params.id })
+    .then(() => res.status(200).json({message: "Objet supprimé !"}))
+    .catch(error => res.status(400).json({ error }));
+});
+
 // Route GET
 
 app.get('/api/stuff/:id', (req, res, next) => {
